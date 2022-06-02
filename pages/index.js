f7f23eeabc67cebd767abe66152008c58ff8aa92
eugene <eugenefromrus@gmail.com>
2022-06-02 20:14:52 +1000

    обновление стилей

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          PRODUCTION
+          DEV
         </h1>
 
         <p className={styles.description}>
@@ -58,7 +58,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          PRODUCTION
+          DEV
         </a>
       </footer>
     </div>
