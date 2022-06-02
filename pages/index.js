42c9d12034e1ecec4311a2f383619c1ffbcde3d3
eugene <eugenefromrus@gmail.com>
2022-06-02 20:12:52 +1000

    обновление шпки и футера

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          PRODUCTION
         </h1>
 
         <p className={styles.description}>
@@ -58,10 +58,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          PRODUCTION
         </a>
       </footer>
     </div>
